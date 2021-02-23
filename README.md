cb45bdbbc562375c70e6da1b6fca7239c00a510a
ahogappa <ahogappa@gmail.com>
2021-02-23 18:30:51 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# search-in-search README
+
+1. Search keywords.
+2. Execute 'Search in Search'.
+3. The file paths of the search result will be copied to 'filesToInclude'.
---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext): void {
+	let disposable = vscode.commands.registerCommand(
+		"search-in-search.searchInSearch",
+		async (): Promise<void> => {
+			await searchInSearch();
+		}
+	);
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
+
+async function searchInSearch(): Promise<void> {
+	await vscode.commands.executeCommand("search.action.copyAll");
+	const projectPath = vscode.workspace.rootPath;
+	const clipBoardResult = await vscode.env.clipboard.readText();
+	let paths: string[] = [];
+
+	if (projectPath === undefined) { return; }
+
+	await vscode.commands.executeCommand("workbench.action.findInFiles", {
+		query: "",
+		filesToInclude: Array.from(clipBoardResult.matchAll(RegExp(`${projectPath}/(.*)`, "g")), m => m[1]).join(", "),
+	});
+}
+
